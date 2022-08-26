9c242ef40ed8cc5e7fa1893ce1d06f417313e78d
Sergey Nuyanzin <snuyanzin@gmail.com>
2022-08-26 13:02:10 +0200

    StreamPhysicalPythonGroupWindowAggregateRule

--- flink-table/flink-table-planner/src/main/java/org/apache/flink/table/planner/plan/rules/physical/stream/StreamPhysicalPythonGroupWindowAggregateRule.java
+++ flink-table/flink-table-planner/src/main/java/org/apache/flink/table/planner/plan/rules/physical/stream/StreamPhysicalPythonGroupWindowAggregateRule.java
@@ -49,15 +49,20 @@ import java.util.List;
  */
 public class StreamPhysicalPythonGroupWindowAggregateRule extends ConverterRule {
 
+    public static final Config DEFAULT_CONFIG =
+            Config.EMPTY
+                    .as(Config.class)
+                    .withConversion(
+                            FlinkLogicalWindowAggregate.class,
+                            FlinkConventions.LOGICAL(),
+                            FlinkConventions.STREAM_PHYSICAL(),
+                            "StreamPhysicalPythonGroupWindowAggregateRule")
+                    .withRuleFactory(StreamPhysicalPythonGroupWindowAggregateRule::new);
     public static final StreamPhysicalPythonGroupWindowAggregateRule INSTANCE =
-            new StreamPhysicalPythonGroupWindowAggregateRule();
+            new StreamPhysicalPythonGroupWindowAggregateRule(DEFAULT_CONFIG);
 
-    private StreamPhysicalPythonGroupWindowAggregateRule() {
-        super(
-                FlinkLogicalWindowAggregate.class,
-                FlinkConventions.LOGICAL(),
-                FlinkConventions.STREAM_PHYSICAL(),
-                "StreamPhysicalPythonGroupWindowAggregateRule");
+    private StreamPhysicalPythonGroupWindowAggregateRule(Config config) {
+        super(config);
     }
 
     @Override
