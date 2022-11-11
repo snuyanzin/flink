a2559e2bd065063f819479664fbb9866fbaaa4ac
Sergey Nuyanzin <snuyanzin@gmail.com>
2022-11-11 13:39:51 +0100

    Calcite 1.31.0 till  Remove unreachable condition in CsvTableScan

--- flink-table/flink-table-planner/src/main/java/org/apache/calcite/rex/RexSimplify.java
+++ flink-table/flink-table-planner/src/main/java/org/apache/calcite/rex/RexSimplify.java
@@ -2247,7 +2247,7 @@ public class RexSimplify {
             return operand;
         }
         if (RexUtil.isLosslessCast(operand)) {
-            // x :: y below means cast(x as y) (which is PostgreSQL-specifiic cast by the way)
+            // x :: y below means cast(x as y) (which is PostgreSQL-specific cast by the way)
             // A) Remove lossless casts:
             // A.1) intExpr :: bigint :: int => intExpr
             // A.2) char2Expr :: char(5) :: char(2) => char2Expr
@@ -3045,7 +3045,9 @@ public class RexSimplify {
          * comparison.
          */
         private static boolean simpleSarg(Sarg sarg) {
-            return sarg.isPoints() || RangeSets.isOpenInterval(sarg.rangeSet);
+            return sarg.isPoints()
+                    || RangeSets.isOpenInterval(sarg.rangeSet)
+                    || sarg.isComplementedPoints();
         }
 
         /**
