95c7390536429a4a758feee324a45cf1c157c353
Sergey Nuyanzin <snuyanzin@gmail.com>
2023-11-25 22:47:19 +0100

    add jdk21 to common.sh

--- flink-end-to-end-tests/test-scripts/common_docker.sh
+++ flink-end-to-end-tests/test-scripts/common_docker.sh
@@ -57,6 +57,10 @@ function build_image() {
     if [[ ${PROFILE} == *"jdk17"* ]]; then
         java_version=17
     fi
+    if [[ ${PROFILE} == *"jdk21"* ]]; then
+        java_version=17
+    fi
+
 
     cd flink-docker
     ./add-custom.sh -u ${file_server_address}:9999/flink.tgz -n ${image_name} -j ${java_version}
