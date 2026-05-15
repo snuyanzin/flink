2fb6f2c34d523645dda2986f1795d09c5a029d9d
Sergey Nuyanzin <snuyanzin@gmail.com>
2026-05-15 20:00:18 +0200

    fix

--- flink-table/flink-table-planner/src/main/java/org/apache/flink/table/planner/plan/schema/TimeIndicatorRelDataType.java
+++ flink-table/flink-table-planner/src/main/java/org/apache/flink/table/planner/plan/schema/TimeIndicatorRelDataType.java
@@ -1,9 +1,31 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.flink.table.planner.plan.schema;
 
 import org.apache.calcite.rel.type.RelDataTypeSystem;
 import org.apache.calcite.sql.type.BasicSqlType;
 import org.apache.calcite.sql.type.SqlTypeName;
 
+/**
+ * Creates a time indicator type for event-time or processing-time, but with similar properties as a
+ * basic SQL type.
+ */
 public class TimeIndicatorRelDataType extends BasicSqlType {
     private final boolean isEventTime;
     private final BasicSqlType originalType;
