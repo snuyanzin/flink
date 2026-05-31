f4d3587167e33d03b52bb762bd91d38474413eb4
Sergey Nuyanzin <snuyanzin@gmail.com>
2026-05-31 22:39:03 +0200

    till CALCITE-6856]

--- flink-table/flink-table-planner/src/main/java/org/apache/calcite/sql2rel/SqlToRelConverter.java
+++ flink-table/flink-table-planner/src/main/java/org/apache/calcite/sql2rel/SqlToRelConverter.java
@@ -2352,6 +2352,7 @@ public class SqlToRelConverter {
 
     private RexNode convertOver(Blackboard bb, SqlNode node) {
         SqlCall call = (SqlCall) node;
+        bb.getValidator().deriveType(bb.scope, call);
         SqlCall aggCall = call.operand(0);
         boolean ignoreNulls = false;
         switch (aggCall.getKind()) {
