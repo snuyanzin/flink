b7b2ae53ecfba908446d7013a25e2688a83f98b1
Sergey Nuyanzin <snuyanzin@gmail.com>
2026-09-19 19:28:49 +0200

    fix

--- tools/ci/free_disk_space.sh
+++ tools/ci/free_disk_space.sh
@@ -46,6 +46,7 @@ sudo apt-get remove -y '^mysql-.*' || true
 sudo apt-get remove -y '^postgresql-.*' || true
 sudo apt-get remove -y '^g\+\+-.*' '^clang-.*' '^libclang-.*' || true
 sudo apt-get remove -y '^gfortran-.*' '^libruby.*' || true
+sudo apt-get remove -y temurin-8-jdk || true
 sudo apt-get remove -y '^libllvm.*' '^libclang1.*' snapd python3-botocore podman buildah skopeo mecab-ipadic gh git-lfs || true
 sudo apt-get remove -y azure-cli google-cloud-sdk google-chrome-stable google-cloud-cli firefox microsoft-edge-stable powershell mono-devel libgl1-mesa-dri || true
 sudo apt-get autoremove -ysudo apt-get autoremove -y || true
