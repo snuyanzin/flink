e2d03554ab7af8b0028474043a9ae359425f8bc1
Sergey Nuyanzin <snuyanzin@gmail.com>
2026-09-19 19:07:40 +0200

    fix

--- tools/ci/free_disk_space.sh
+++ tools/ci/free_disk_space.sh
@@ -47,7 +47,7 @@ sudo apt-get remove -y '^postgresql-.*'
 sudo apt-get remove -y '^g\+\+-.*' '^clang-.*' '^libclang-.*'
 sudo apt-get remove -y '^gfortran-.*' '^libruby.*'
 sudo apt-get remove -y '^libllvm.*' '^libclang1.*' snapd python3-botocore podman buildah skopeo mecab-ipadic gh git-lfs
-sudo apt-get remove -y azure-cli google-cloud-sdk hhvm google-chrome-stable google-cloud-cli firefox microsoft-edge-stable powershell mono-devel libgl1-mesa-dri
+sudo apt-get remove -y azure-cli google-cloud-sdk google-chrome-stable google-cloud-cli firefox microsoft-edge-stable powershell mono-devel libgl1-mesa-dri
 sudo apt-get autoremove -ysudo apt-get autoremove -y
 sudo apt-get clean
 df -h
